9f295250f41d3deb4b1caa2b5a40a310cd363f26
Maurice Tadros <maurice.tadros@gmail.com>
2019-11-26 13:16:41 -0700

    Created an empty plugin

---
+++ beans-wpforms-uikit3-addon.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Beans using uikit 3 and wp forms
+ *
+ * https://getuikit.com/v2/
+ *
+ * @package    Beans_WPForms_Uikit3_Addon
+ * @since      0.1
+ * @author     Maurice Tadros, Yaidel Ferralize, Disnel Rodriguez
+ * @link       https://bowriverstudio.com
+ * @license    GNU-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name:    Beans - UiKit 3.2  -  WP Forms - Addon
+ * Description:     An Addon for wpforms that uses Beans and uikit 3: Plugin is free as beer and In Development.
+ *
+ */
+namespace Beans_WPForms_Uikit3_Addon;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Hello, Hello, Hello, what\'s going on here then?' );
+}
+
+
+
+
